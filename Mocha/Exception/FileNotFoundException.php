a721a4ef0990b7ab3628874309aa49e78c7d6c83
lukehayes <lukepurplehayes@gmail.com>
2019-06-14 13:30:21 +0100

    Added FileNotFoundException

---
+++ Mocha/Exception/FileNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mocha\Exception;
+
+/**
+ * Custom execption for when a file could not be found
+ */
+class FileNotFoundException extends \Exception {
+
+    public function __construct($file) {
+        $message = "The file <strong>". $file . "</strong> could not be found.";
+        parent::__construct($message);
+    }
+}
+
