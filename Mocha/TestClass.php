246f4e2476b1951f3d355c698b98232caea9ac07
lukehayes <lukepurplehayes@gmail.com>
2019-06-14 09:52:59 +0100

    Addee test calss

---
+++ Mocha/TestClass.php
@@ -0,0 +1,17 @@
+<?php 
+namespace Mocha; 
+
+/**
+ * Test Class
+ * @mocha-page hello
+ * @mocha-date 12.12.12
+ */
+class TestClass 
+{
+	/**
+	 * Test Method
+	 * @mocha-coolthing myMethodPage
+	 */
+	public function test(){}
+}
+
