f0bee907891683bdeda64c2491e67e762c367648
lukehayes <lukepurplehayes@gmail.com>
2019-06-14 08:59:51 +0100

    Added ParserInterface

---
+++ Mocha/Annotation/ParserInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mocha\Annotation;
+
+/**
+ * Parser Interface
+ */
+interface ParserInterface
+{
+    
+    /**
+     * Parse something
+     */
+    public function parse();
+    
+}
