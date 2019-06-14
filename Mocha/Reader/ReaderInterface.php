6ea3a3ed2eb96638a63cdf49c73a99a9b070c0d0
lukehayes <lukepurplehayes@gmail.com>
2019-06-14 13:05:48 +0100

    Added Mocha\Reader namespace

---
+++ Mocha/Reader/ReaderInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mocha\Reader;
+
+/**
+ * Reader Interface for reading in data
+ */
+interface ReaderInterface {
+
+    /**
+     * Read in some data
+     * @return string Read data in
+     */
+    public function read();
+}
