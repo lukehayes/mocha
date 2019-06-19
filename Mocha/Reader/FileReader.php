3ac43d819af6b0f9c0cb825f789b2f44d2921f86
lukehayes <lukepurplehayes@gmail.com>
2019-06-19 21:44:17 +0100

    FileReader now has constructor that takes a file string

--- Mocha/Reader/FileReader.php
+++ Mocha/Reader/FileReader.php
@@ -9,20 +9,30 @@ use Mocha\Exception\FileNotFoundException;
 class FileReader implements ReaderInterface {
 
     /**
+     * Name of the file to read
+     * @var
+     */
+    private $file = NULL;
+
+    public function __construct($file) {
+        $this->file = $file;
+    }
+
+    /**
      * Open a file, read it and return as a string
      * @param  string $file Path to file
      * @return string Read data in
      *
      * @throws Mocha\Exception\FileNotFoundException
      */
-    public function read($file) : string {
+    public function read() : string {
 
-        if ( ! file_exists($file) ) {
-            throw new FileNotFoundException($file);
+        if ( ! file_exists($this->file) ) {
+            throw new FileNotFoundException($this->file);
         }
 
-        $handle = fopen($file, "r");
-        $contents = fread($handle, filesize($file));
+        $handle = fopen($this->file, "r");
+        $contents = fread($handle, filesize($this->file));
         fclose($handle);
 
         return $contents;
--- Mocha/Reader/ReaderInterface.php
+++ Mocha/Reader/ReaderInterface.php
@@ -8,8 +8,7 @@ interface ReaderInterface {
 
     /**
      * Read in some data
-     * @param  string $file Path to file
      * @return string Read data in
      */
-    public function read($file) : string;
+    public function read() : string;
 }
