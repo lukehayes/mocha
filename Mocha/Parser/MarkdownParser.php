d1f4ad048512e3c63b7bf67a99c8cd8f9e118f46
lukehayes <lukepurplehayes@gmail.com>
2019-12-06 17:27:53 +0000

    Added start of Markdown Parser class

---
+++ Mocha/Parser/MarkdownParser.php
@@ -0,0 +1,68 @@
+<?php
+namespace Mocha\Parser;
+
+/**
+ * Class that can parse Markdown and convert it
+ * into its HTML equivalent
+ *
+ * @package Mocha\Parser
+ */
+class MarkdownParser implements ParserInterface {
+    
+    /**
+     * The markdown content that we want to parse
+     *
+     * @var string | NULL
+     */
+    private $markdown = NULL;
+    
+    /**
+     * Individual lines of markdown
+     *
+     * @var array
+     */
+    private $lines = [];
+
+    public function __construct($markdown) {
+
+        if( ! is_string($markdown) ) {
+            throw new \Exception("Data passed into constructor must be a string!");
+        }
+
+        $this->markdown = $markdown;
+    }
+    
+    /**
+     * Parse the markdown content
+     *
+     * @return string
+     */
+    public function parse() {
+        $parsed_markdown = [];
+        $lines = $this->split();
+        
+        // TODO
+    }
+    
+
+    /**
+     * Split the markdown content into individual lines
+     *
+     * @return array
+     */
+    private function split() : array {
+
+        // Split markdown into lines
+        $split = preg_split('#\n#',$this->markdown);
+        
+        // Filter through split to remove empty lines
+        $this->lines = array_filter($split, function($line) {
+            if( ! empty($line) ) {
+                return $line;
+            }
+        });
+
+        return $this->lines;
+    }
+
+}
