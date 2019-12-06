8d1afb7ba4a312d3e890d37c43d5afc022682611
lukehayes <lukepurplehayes@gmail.com>
2019-12-06 17:28:31 +0000

    Updated index

--- index.php
+++ index.php
@@ -3,5 +3,19 @@
 require "vendor/autoload.php";
 
 use Mocha\Annotation\Annotation;
+use Mocha\Annotation\Tag;
+use Mocha\Reader\FileReader;
+use Mocha\Parser\MarkdownParser;
 
 $a = new Annotation();
+$fr = new FileReader("md.md");
+
+$parser = new MarkdownParser($fr->read());
+$parser->parse();
+
+//$matches = [];
+//$res = preg_match_all(Tag::MOCHA_TAG, $fr->read(), $matches, PREG_PATTERN_ORDER);
+
+//dump($matches);
+
+
