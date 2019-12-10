f5b7c875aa6c43e7343f96d7bb8bf14f678f04dc
lukehayes <lukepurplehayes@gmail.com>
2019-12-10 15:28:27 +0000

    Added default header and footer templates to project

---
+++ front-end/footer.php
@@ -0,0 +1,4 @@
+<!-- Mocha Default Footer -->
+
+</body>
+</html>
---
+++ front-end/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mocha</title>
+</head>
+<body>
+<!-- Mocha Default Header -->
