7bf13951555e8523d3d4938757d97bfa3924a22a
lukehayes <lukepurplehayes@gmail.com>
2019-12-11 09:55:13 +0000

    Mocha/Parser/ConfigParser now has a  property and constructor

--- Mocha/Parser/ConfigParser.php
+++ Mocha/Parser/ConfigParser.php
@@ -9,13 +9,25 @@ use Symfony\Component\Yaml\Exception\ParseException;
 class ConfigParser implements ParserInterface {
 
     /**
+     * Path to the Mocha config file that
+     * we want to parse
+     *
+     * @var
+     */
+    private $configFile = NULL;
+
+    public function __construct($configFile = "mocha-config.yaml") {
+        $this->configFile = $configFile;
+    }
+
+    /**
      * Parse a Mocha Config file
      * @return array The data that has been parsed
      */
     public function parse() :  array {
 
         try {
-            $config = Yaml::parseFile('mocha-config.yaml');
+            $config = Yaml::parseFile($this->configFile);
         } catch (ParseException $exception) {
             printf('Unable to parse the config file: %s', $exception->getMessage());
         }
