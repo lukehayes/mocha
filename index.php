e4de41d6ab63395f2d5b2297b42d20d554104284
lukehayes <lukepurplehayes@gmail.com>
2019-12-11 09:54:42 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
-
 require "vendor/autoload.php";
 
 use Mocha\Annotation\Annotation;
 use Mocha\Annotation\Tag;
 use Mocha\Reader\FileReader;
 use Mocha\Parser\MarkdownParser;
+use Mocha\Parser\ConfigParser;
+
+$configObject = new ConfigParser();
+$config = $configObject->parse();
 
 $a = new Annotation();
-$fr = new FileReader("md.md");
+$fr = new FileReader($config["template_dir"] . "/hello.md");
 
+dump(file_get_contents(dirname(__FILE__) . "/" . $config["template_dir"] . "/hello.md"));
 $parser = new MarkdownParser($fr->read());
-$parser->parse();
+dump($fr);
+dump($parser->parse());
 
 //$matches = [];
 //$res = preg_match_all(Tag::MOCHA_TAG, $fr->read(), $matches, PREG_PATTERN_ORDER);
