12d4ee94cb665f909117bcd6a528de757b73f086
lukehayes <lukepurplehayes@gmail.com>
2020-04-15 15:43:20 +0100

    Index now reading markdown

--- index.php
+++ index.php
@@ -1,22 +1,14 @@
 <?php
 require "vendor/autoload.php";
 
-use Mocha\Annotation\Annotation;
-use Mocha\Annotation\Tag;
 use Mocha\Reader\FileReader;
-use Mocha\Parser\MarkdownParser;
 use Mocha\Parser\ConfigParser;
+
 
 $configObject = new ConfigParser();
 $config = $configObject->parse();
 
-$a = new Annotation();
-
 $fr = new FileReader("front-end/hello.md");
-dump($fr->read());
+$parsedown = new Parsedown();
 
-$parser = new MarkdownParser($fr->read());
-$res = $parser->parse();
-
-dump($res);
-
+echo $parsedown->text($fr->read());
