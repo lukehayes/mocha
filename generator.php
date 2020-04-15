e1e631daec7cf60a024be5a3535589ee402c5b9d
lukehayes <lukepurplehayes@gmail.com>
2020-04-15 18:33:21 +0100

    Added generator.php

---
+++ generator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Generator class to get the general idea working
+ */
+
+/**
+ * Generate a site build
+ * 
+ * @param $build_dir    The name of the directory to build in
+ * @param $pages_dir    The name of the directory where the markdown is stored
+ */
+function generate($build_dir, $pages_dir) {
+    echo "Generating site build...";
+}
+
+
