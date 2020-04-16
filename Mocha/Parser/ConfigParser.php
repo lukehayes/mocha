c7469ef3cb3e613acb8108ca4602bb997eb89bc5
lukehayes <lukepurplehayes@gmail.com>
2020-04-16 12:12:49 +0100

    Rewrote ConfigParser to use static factory method and __get magic method

--- Mocha/Parser/ConfigParser.php
+++ Mocha/Parser/ConfigParser.php
@@ -18,23 +18,58 @@ class ConfigParser implements ParserInterface {
      */
     private $configFile = NULL;
 
+    /**
+     * @var array | null   All parsed values from the configuration file
+     */
+    public $config = NULL;
+
+    /**
+     * Constructor
+     *
+     * @param string $configFile    The name of the configuration file that will be parsed
+     * 
+     * @return string    Result of parsing the configuration file
+     */
     public function __construct($configFile = "mocha-config.yaml") {
         $this->configFile = $configFile;
     }
+    
+    /**
+     * Parse a Mocha configuration file
+     *
+     * @property-read string $name    The value to retrieve from the configuration file
+     * 
+     * @return string|null    Result of parsing the configuration file
+     */
+    public function __get($name) {
+        return array_key_exists($name, $this->config) ?
+            $this->config[$name] : null;
+    }
+    
+    /**
+     * Static factory method for creating a ConfigParser object
+     *
+     * @return ConfigParser
+     */
+    public static function create() {
+        return new ConfigParser();
+    }
 
     /**
-     * Parse a Mocha Config file
+     * Parse a Mocha configuration file
      *
-     * @return array    Result of parsing the configuration file
+     * @throws ParseException
+     *
+     * @return object    Result of parsing the configuration file
      */
-    public function parse() :  array {
+    public function parse() : object {
 
         try {
-            $config = Yaml::parseFile($this->configFile);
+            $this->config = Yaml::parseFile($this->configFile);
         } catch (ParseException $exception) {
-            printf('Unable to parse the config file: %s', $exception->getMessage());
+            printf('Unable to parse the configuration file: %s', $exception->getMessage());
         }
-
-        return $config;
+        
+        return $this;
     }
 }
