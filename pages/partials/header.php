8cc5dae8008fb9cc53d729f4185338fdcc28fa84
lukehayes <lukepurplehayes@gmail.com>
2020-04-20 18:33:42 +0100

    Added test css to header.php

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Mocha</title>
+
+    <style>
+    h1 {color:red;}
+    p {color:blue;}
+    </style>
+
 </head>
 <body>
 <!-- Mocha Default Header -->
