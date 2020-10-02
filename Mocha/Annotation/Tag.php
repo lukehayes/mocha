b178aaac6676bc0f6e0db9021bcc99e6d8440304
lukehayes <lukepurplehayes@gmail.com>
2020-10-02 17:42:24 +0100

    Tag::MOCHA_TAG now looks for dates also

--- Mocha/Annotation/Tag.php
+++ Mocha/Annotation/Tag.php
@@ -2,20 +2,20 @@
 namespace Mocha\Annotation;
 
 /**
- * Tag holds const strings that represent
- * possible Mocha tags
+ * Tag class holds regular expressions that should match
+ * tags that have special meaning inside Mocha.
  */
 class Tag {
 
 	/**
 	 * Any '@mocha-'' tag
 	 */
-	const MOCHA_TAG = "/(@mocha-\w+)\s(\w+)/";
+	const MOCHA_TAG = "/@mocha-(\w+)\s([a-z]+|\d{2}-\d{2}-\d{2,4})/";
 
 	/**
 	 * Represents the '@mocha-date' annotation
 	 */
-	const MOCHA_DATE = "/(@mocha-date)\s(\d{2}\/){2}\d{2,4} /";
+	const MOCHA_DATE = "/(@mocha-date)\s(\d{2}-\d{2}-\d{2,4})/";
 
 	/**
 	 * Represents the '@mocha-title' annotation
