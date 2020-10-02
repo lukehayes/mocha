1a548ddc2673336d6779fd502b6ffa0833cab2bd
lukehayes <lukepurplehayes@gmail.com>
2020-10-02 13:44:14 +0100

    Added $options param for AnnotationMatch constructor

--- Mocha/Annotation/AnnotationMatch.php
+++ Mocha/Annotation/AnnotationMatch.php
@@ -4,45 +4,59 @@ use Mocha\Annotation;
 /**
  * An object to hold any match that is found when parsing
  */
-class AnnotationMatch {
-
+class AnnotationMatch 
+{
     /**
-     * The name of the found match
-     *
-     * @var
+     * @var The name of the found match.
      */
     private $title = NULL;
 
     /**
-     * The value of the found match
-     *
-     * @var
+     * @var The value of the found match.
      */
     private $value = NULL;
 
-    public function __construct($title, $value) {
+    /**
+     * @var Extra options for the particular annotation.
+     */
+    private $options = NULL;
+
+    /**
+     * Constructor.
+     * @param string $title
+     * @param string $value
+     * @param string $options
+     */
+    public function __construct($title, $value, $options) {
         $this->title = $title;
         $this->value = $value;
+        $this->options = $options;
     }
     
     /**
      * Title getter for an AnnotationMatch
-     *
      * @return string | NULL
      */
     public function getTitle() {
         return $this->title;
 
     }
+
     /**
      * Value getter for an AnnotationMatch
-     *
      * @return string | NULL
      */
     public function getValue() {
         return $this->value;
     }
 
+    /**
+     * Options getter for an AnnotationMatch
+     * @return string | NULL
+     */
+    public function getOptions() {
+        return $this->options;
+    }
 }
 
 
